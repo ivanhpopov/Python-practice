24464484b8d2958728c699ca1d766b920d5be038
ipopov <140158056+ivanhpopov@users.noreply.github.com>
2023-11-26 17:39:47 +0200

    Finalized the Lists chapter and tuples, on to conditional IF statements.

---
+++ tuples/buffet.py
@@ -0,0 +1,9 @@
+buffet = ('omlete', 'pizza', 'chocolate', 'cookies', 'beef')
+for buff in buffet:
+    print(buff)
+#buffet[0] = 'error'
+
+print("\nNew menu:")
+buffet = ('omlete', 'pizza', 'chocolate', 'tea', 'whiskey')
+for buff in buffet:
+    print(buff)
