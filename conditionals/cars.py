ffe7f54aa3ad3989253ae24401e4374be7212ec0
ipopov <140158056+ivanhpopov@users.noreply.github.com>
2023-11-27 13:30:00 +0200

    Halfway through the conditional statements chapter. So far nothing new learned, but the way they present everything is phenomenal! The practice tasks make you think and use what you've refreshed.

---
+++ conditionals/cars.py
@@ -0,0 +1,22 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+if 'bmw' in cars:
+    print('BMW is in cars')
+if 'honda' not in cars:
+    print('no honda in list')
+if len(cars) >= 4:
+    print('The list of cars contains four or more vehicles')
+if len(cars) > 3 and len(cars) < 6:
+    print('true')
+if 'audii' in cars or 'toyota' in cars:
+    print('trueee')
+if 'bmw' not in cars:
+    print('BMW is in cars')
+if 'honda' in cars:
+    print('no honda in list')
+if len(cars) > 4:
+    print('The list of cars contains four or more vehicles')
+if len(cars) < 3 and len(cars) > 6:
+    print('true')
+if 'audii' in cars or 'toyota' not in cars:
+    print('trueee')
