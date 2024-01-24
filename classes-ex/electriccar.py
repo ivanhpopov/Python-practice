f940eaf9ac19a8b2f45ef3e245f46a97420ab562
ipopov <140158056+ivanhpopov@users.noreply.github.com>
2024-01-24 17:52:51 +0200

    More on classes.

--- classes-ex/electriccar.py
+++ classes-ex/electriccar.py
@@ -31,20 +31,25 @@ class Car:
             self.odometer_reading += miles
         else:
             print("You can't rollback the odometer reading.")
-
-my_new_car = Car('audi', 'a4', 2024)
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-
-my_new_car.update_odometer(24)
-my_new_car.read_odometer()
-
-my_used_car = Car('subaru', 'outback', 2019)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(1)
-my_used_car.read_odometer()
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    
+    def __init__(self, battery_size=40):
+        """Initialize te battery's attributes."""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+  
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
